0ff3283e55d9537b068079edc40f4f541719e9e2
imlyh <a17038099761@163.com>
2026-05-10 19:08:28 +0800

    修复：改用EntityManager直接创建实体替代ECB确保Parent组件正确挂载

--- Assets/Scripts/Systems/BattalionInitializationSystem.cs
+++ Assets/Scripts/Systems/BattalionInitializationSystem.cs
@@ -2,11 +2,13 @@ using Unity.Entities;
 using Unity.Transforms;
 using Unity.Mathematics;
 using UnityEngine;
+using System.Collections.Generic;
 
 [UpdateInGroup(typeof(InitializationSystemGroup))]
 public partial class BattalionInitializationSystem : SystemBase
 {
-    public static readonly System.Collections.Generic.Dictionary<int, GameObject> GOMap = new();
+    public static readonly Dictionary<int, GameObject> GOMap = new();
+
     protected override void OnCreate()
     {
         EntityManager.CreateSingleton<PlayerCommandData>();
@@ -22,52 +24,50 @@ public partial class BattalionInitializationSystem : SystemBase
         Object.DestroyImmediate(temp);
 
         int layer = LayerMask.NameToLayer("Unit");
-        CreateBattalion(new float3(4,0,10), BattalionOwner.Player, "PBN_A", cubeMesh, mat, layer);
-        CreateBattalion(new float3(4,0,7),  BattalionOwner.Player, "PBN_B", cubeMesh, mat, layer);
-        CreateBattalion(new float3(25,0,10), BattalionOwner.Enemy, "EBN_A", cubeMesh, mat, layer);
-        CreateBattalion(new float3(25,0,13), BattalionOwner.Enemy, "EBN_B", cubeMesh, mat, layer);
+        CreateBattalion(new float3(4,0,10), BattalionOwner.Player, "PBN_A", mat, layer);
+        CreateBattalion(new float3(4,0,7),  BattalionOwner.Player, "PBN_B", mat, layer);
+        CreateBattalion(new float3(25,0,10), BattalionOwner.Enemy, "EBN_A", mat, layer);
+        CreateBattalion(new float3(25,0,13), BattalionOwner.Enemy, "EBN_B", mat, layer);
         Debug.Log("[ECS] Battalions created");
     }
 
-    void CreateBattalion(float3 pos, BattalionOwner owner, string name, Mesh mesh, Material mat, int layer)
+    void CreateBattalion(float3 pos, BattalionOwner owner, string name, Material mat, int layer)
     {
-        var ecb = new EntityCommandBuffer(Unity.Collections.Allocator.Temp);
-        var e = ecb.CreateEntity();
-        ecb.SetName(e, name);
-        ecb.AddComponent(e, LocalTransform.FromPosition(pos));
-        ecb.AddComponent(e, new BattalionData
+        var em = EntityManager;
+        var e = em.CreateEntity();
+        em.SetName(e, name);
+        em.AddComponentData(e, LocalTransform.FromPosition(pos));
+        em.AddComponentData(e, new BattalionData
         {
             owner=owner, state=BattalionState.Idle, moveSpeed=4f,
             detectionRange=1.3f, bobHeight=0.2f, bobFrequency=8f,
             bobPhase=UnityEngine.Random.Range(0f,100f)
         });
-        ecb.AddBuffer<BattalionPathPoint>(e);
-        var children = ecb.AddBuffer<Child>(e);
+        em.AddBuffer<BattalionPathPoint>(e);
+        var ch = em.AddBuffer<Child>(e);
 
         float s = 0.55f;
         var off = new float3[]{ new(-s/2,0,-s/2),new(s/2,0,-s/2),new(-s/2,0,s/2),new(s/2,0,s/2) };
         for (int i = 0; i < 4; i++)
         {
-            var se = ecb.CreateEntity();
-            ecb.SetName(se, $"{name}_S{i}");
-            ecb.AddComponent(se, new Parent{Value=e});
-            ecb.AddComponent(se, LocalTransform.FromPosition(off[i]));
-            ecb.AddComponent(se, new SoldierData{
+            var se = em.CreateEntity();
+            em.SetName(se, $"{name}_S{i}");
+            em.AddComponentData(se, LocalTransform.FromPosition(off[i]));
+            em.AddComponentData(se, new Parent{Value=e});
+            em.AddComponentData(se, new SoldierData{
                 attackRange=1.5f,attackCooldown=1.5f,dashSpeed=10f,dashHeight=0.25f,formationOffset=off[i]
             });
-            children.Add(new Child{Value=se});
+            ch.Add(new Child{Value=se});
 
             var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
             go.name = $"{name}_S{i}_GO";
             go.transform.localScale = Vector3.one * 0.35f;
-            go.transform.position = new Vector3(pos.x + off[i].x, pos.y, pos.z + off[i].z);
+            go.transform.position = new Vector3(pos.x+off[i].x, pos.y, pos.z+off[i].z);
             go.layer = layer;
-            go.tag = owner == BattalionOwner.Player ? "PlayerUnit" : "EnemyUnit";
+            go.tag = owner==BattalionOwner.Player?"PlayerUnit":"EnemyUnit";
             go.GetComponent<MeshRenderer>().material = mat;
             GOMap[go.GetInstanceID()] = go;
-            ecb.AddComponent(se, new EntityLink{goInstanceID=go.GetInstanceID()});
+            em.AddComponentData(se, new EntityLink{goInstanceID=go.GetInstanceID()});
         }
-        ecb.Playback(EntityManager);
-        ecb.Dispose();
     }
 }
