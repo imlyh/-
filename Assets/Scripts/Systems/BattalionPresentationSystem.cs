12cb7d4a61704780d6588b5ffc74bb0434c621c5
imlyh <a17038099761@163.com>
2026-05-10 19:32:17 +0800

    修复：表现系统用LocalTransform实时位置替代固定formationOffset

--- Assets/Scripts/Systems/BattalionPresentationSystem.cs
+++ Assets/Scripts/Systems/BattalionPresentationSystem.cs
@@ -10,12 +10,11 @@ public partial class BattalionPresentationSystem : SystemBase
     protected override void OnUpdate()
     {
         var map = BattalionInitializationSystem.GOMap;
-        foreach (var (sdRef, linkRef) in SystemAPI.Query<RefRO<SoldierData>, RefRO<EntityLink>>())
+        foreach (var (sdRef, ltxRef, linkRef) in SystemAPI.Query<RefRO<SoldierData>, RefRO<LocalTransform>, RefRO<EntityLink>>())
         {
-            var sd = sdRef.ValueRO;
             if (!map.TryGetValue(linkRef.ValueRO.goInstanceID, out var go)) continue;
-            var batLTW = SystemAPI.GetComponent<LocalToWorld>(sd.battalionEntity);
-            go.transform.position = (Vector3)batLTW.Position + (Vector3)sd.formationOffset;
+            var batLTW = SystemAPI.GetComponent<LocalToWorld>(sdRef.ValueRO.battalionEntity);
+            go.transform.position = (Vector3)batLTW.Position + (Vector3)ltxRef.ValueRO.Position;
         }
     }
 }
